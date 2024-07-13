9cc003a4f6f3002844d33c80f208e775579ad48b
AliceGuntli <alice@guntli.com>
2024-07-13 18:29:47 +0200

    basic maneuvering pure rotation (magic values)

--- diff_to_ackermann/src/diff_to_ackermann_node.cpp
+++ diff_to_ackermann/src/diff_to_ackermann_node.cpp
@@ -13,6 +13,7 @@ class DiffToAck : public rclcpp::Node
       publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
       subscriber_ = this->create_subscription<geometry_msgs::msg::Twist>(
         "diffdrive_cmd", 10, std::bind(&DiffToAck::twistCallback, this, std::placeholders::_1));
+      forward = 0;
     }
 
   private:
@@ -25,19 +26,30 @@ class DiffToAck : public rclcpp::Node
       geometry_msgs::msg::Twist ackermann_msg;
       
       // Calculate steering angle
-      if (std::abs(msg->linear.x) < 1e-6) {
-        ackermann_msg.angular.z = 0; // Pure rotation case
+      if (std::abs(msg->linear.x) < 0.01 && std::abs(msg->angular.z)>0.01) {
+        
+        if (forward <= 10){
+          ackermann_msg.linear.x = 0.5;
+        } else if (forward <= 30) {
+          ackermann_msg.linear.x = -1.0;
+          ackermann_msg.angular.z = 20*msg->angular.z; // Pure rotation case
+        } else if (forward <= 40) {
+          ackermann_msg.linear.x = 0.5;
+          if (forward == 40){
+            forward = 0;
+          }
+        }
+        forward++;
       } else {
         ackermann_msg.angular.z = std::atan2(wheelbase * msg->angular.z, std::abs(msg->linear.x));
+        ackermann_msg.linear.x = msg->linear.x; // Linear velocity remains the same
       }
-      
-      ackermann_msg.linear.x = msg->linear.x; // Linear velocity remains the same
-
       publisher_->publish(ackermann_msg);
     }
 
     rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
     rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr subscriber_;
+    int forward; 
 };
 
 int main(int argc, char * argv[])
